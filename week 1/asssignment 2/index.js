49e2d4ebe24728d96da3d47be141474da1b3a28f
vishalpatil12014017 <87421912+vishalpatil12014017@users.noreply.github.com>
2021-12-25 10:46:01 +0530

    infinite scrollbar

---
+++ week 1/asssignment 2/index.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container');
+let counter = 1;
+function loadImages(numImages = 8) {
+    let i = 0;
+    while (i < numImages) {
+        const div = document.createElement('div');
+        div.textContent = `Box ${counter}`
+        container.appendChild(div);
+        console.log(counter,i);
+        counter++;
+        i++;
+    }
+}
+
+loadImages()
+window.addEventListener('scroll', () => {
+    // console.log(window.innerHeight)
+    // console.log(window.scrollY)
+    if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
+        setTimeout(() => {
+            loadImages()
+
+        }, 1000)
+    }
+})
